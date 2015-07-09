41147b10741807f4c499c1b283e7cfdca5585a9a
Hedayat Vatankhah <hedayat.fwd@gmail.com>
2015-07-10 01:03:57 +0430

    Implement base skelton for network

--- qtconnection.cpp
+++ qtconnection.cpp
@@ -6,198 +6,292 @@
  */
 
 #include <QObject>
+#include <QTcpSocket>
+#include <cassert>
 #include <tgl.h>
 
-struct connection {
-        tgl_dc *dc;
-        tgl_session *session;
+static connection *create_connection(tgl_state *tls, const char *host, int port,
+    tgl_session *session, tgl_dc *dc, mtproto_methods *methods);
+static int write_out (connection *c, const void *_data, int len);
+static int read_in (connection *c, void *_data, int len);
+static int read_in_lookup (connection *c, void *_data, int len);
+static void incr_out_packet_num (connection *c);
+static tgl_dc *get_dc(connection *c);
+static tgl_session *get_session(connection *c);
+static void flush_out(connection *c);
+static void conn_free(connection *c);
+
+tgl_net_methods conn_methods =
+{
+    write_out,
+    read_in,
+    read_in_lookup,
+    flush_out,
+    incr_out_packet_num,
+    conn_free,
+    get_dc,
+    get_session,
+    create_connection
 };
 
 
-int write_out (struct connection *c, const void *_data, int len) {
-    qDebug(__PRETTY_FUNCTION__);
-//  struct tgl_state *TLS = c->TLS;
-//  const unsigned char *data = _data;
-//  if (!len) { return 0; }
-//  assert (len > 0);
-  int x = 0;
-//  while (len) {
-//    if (c->out_tail->end - c->out_tail->wptr >= len) {
-//      memcpy (c->out_tail->wptr, data, len);
-//      c->out_tail->wptr += len;
-//      c->out_bytes += len;
-//      return x + len;
-//    } else {
-//      int y = c->out_tail->end - c->out_tail->wptr;
-//      assert (y < len);
-//      memcpy (c->out_tail->wptr, data, y);
-//      x += y;
-//      len -= y;
-//      data += y;
-////      struct connection_buffer *b = new_connection_buffer (1 << 20);
-////      c->out_tail->next = b;
-////      b->next = 0;
-////      c->out_tail = b;
-//      c->out_bytes += y;
-//    }
-//  }
-  return x;
+struct connection: public QObject
+{
+    private:
+        Q_OBJECT;
+
+    public:
+        connection(tgl_state *tls, tgl_session *s, tgl_dc *dc,
+            mtproto_methods *mp, QObject *parent = NULL);
+
+        void connect_to_server(const char *host, int port);
+        int write(const void *_data, int len);
+        int read(void *_data, int len);
+        void flush() { socket.flush(); }
+
+    public:
+        tgl_state *tl_state;
+        tgl_session *session;
+        tgl_dc *dc;
+
+    private:
+        QTcpSocket socket;
+        mtproto_methods *methods;
+
+    private slots:
+        void connected();
+        void disconnected();
+        void error(QAbstractSocket::SocketError socketError);
+        void host_found();
+//        void proxyAuthenticationRequired(const QNetworkProxy & proxy,
+//            QAuthenticator * authenticator)
+        void state_changed(QAbstractSocket::SocketState socketState);
+        void about_to_close();
+        void bytes_written(qint64 bytes);
+        void read_channel_finished();
+        void ready_read();
+
+    private:
+        void fail()
+        {
+            int new_port = rotate_port();
+            QByteArray addr = socket.peerName().toUtf8();
+            connect_to_server(addr.data(), new_port);
+        }
+
+        int rotate_port()
+        {
+            switch (socket.peerPort())
+            {
+                case 443:
+                    return 80;
+                case 80:
+                    return 25;
+                case 25:
+                    return 443;
+            }
+        }
+
+        void try_rpc_read();
+};
+
+connection::connection(tgl_state *tls, tgl_session *s, tgl_dc *dc,
+    mtproto_methods *mp, QObject *parent) :
+        QObject(parent), tl_state(tls), socket(this), session(s), dc(dc),
+        methods(mp)
+{
+    socket.setSocketOption(QAbstractSocket::LowDelayOption, 1);
+    socket.setSocketOption(QAbstractSocket::KeepAliveOption, 1);
+    connect(&socket, SIGNAL(connected()), this, SLOT(connected()));
+    connect(&socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
+    connect(&socket, SIGNAL(error(QAbstractSocket::SocketError)),
+        this, SLOT(error(QAbstractSocket::SocketError)));
+    connect(&socket, SIGNAL(hostFound()), this, SLOT(host_found()));
+    connect(&socket, SIGNAL(stateChanged(QAbstractSocket::SocketState)),
+        this, SLOT(state_changed(QAbstractSocket::SocketState)));
+
+    connect(&socket, SIGNAL(aboutToClose()), this, SLOT(about_to_close()));
+    connect(&socket, SIGNAL(bytesWritten(qint64)), this,
+        SLOT(bytes_written(qint64)));
+    connect(&socket, SIGNAL(readChannelFinished()), this,
+        SLOT(read_channel_finished()));
+    connect(&socket, SIGNAL(readyRead()), this, SLOT(ready_read()));
 }
 
-int read_in (struct connection *c, void *_data, int len) {
+void connection::connect_to_server(const char *host, int port)
+{
     qDebug(__PRETTY_FUNCTION__);
-//  unsigned char *data = _data;
-//  if (!len) { return 0; }
-//  assert (len > 0);
-//  if (len > c->in_bytes) {
-//    len = c->in_bytes;
-//  }
-  int x = 0;
-//  while (len) {
-//    int y = c->in_head->wptr - c->in_head->rptr;
-//    if (y > len) {
-//      memcpy (data, c->in_head->rptr, len);
-//      c->in_head->rptr += len;
-//      c->in_bytes -= len;
-//      return x + len;
-//    } else {
-//      memcpy (data, c->in_head->rptr, y);
-//      c->in_bytes -= y;
-//      x += y;
-//      data += y;
-//      len -= y;
-//      void *old = c->in_head;
-//      c->in_head = c->in_head->next;
-//      if (!c->in_head) {
-//        c->in_tail = 0;
-//      }
-////      delete_connection_buffer (old);
-//    }
-//  }
-  return x;
+    socket.connectToHost(host, port);
+    // todo implement pinging the server
+//  c->last_receive_time = tglt_get_double_time ();
+//  c->ping_ev = evtimer_new (TLS->ev_base, ping_alarm, c);
+//  start_ping_timer (c);
+
+    // 5 seconds for inactivity check (failed connection)
 }
 
-int read_in_lookup (struct connection *c, void *_data, int len) {
+int connection::write(const void *_data, int len)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    // buffer data & write
+    return len; // num written
+}
+
+int connection::read(void *_data, int len)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    // read data from buffer to _data
+    return 0;//num read
+}
+
+void connection::connected()
+{
+    qDebug(__PRETTY_FUNCTION__);
+    char byte = 0xef;
+    assert(socket.write(&byte, 1) == 1);
+    socket.flush();
+
+    methods->ready(tl_state, this);
+}
+
+void connection::disconnected()
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::error(QAbstractSocket::SocketError socketError)
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::host_found()
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::state_changed(QAbstractSocket::SocketState socketState)
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::about_to_close()
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::bytes_written(qint64 bytes)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    // write remaining data?!
+}
+
+void connection::read_channel_finished()
+{
+    qDebug(__PRETTY_FUNCTION__);
+}
+
+void connection::ready_read()
+{
+    qDebug(__PRETTY_FUNCTION__);
+    // restart ping timer
+    // read into buffer
+    // if error, fail_connection()
+    // x = readnum
+    int x = 0;
+    if (x)
+        try_rpc_read();
+}
+
+void connection::try_rpc_read()
+{
+    qDebug(__PRETTY_FUNCTION__);
+//  while (1) {
+//    if (c->in_bytes < 1) { return; }
+//    unsigned len = 0;
+//    unsigned t = 0;
+//    assert (tgln_read_in_lookup (c, &len, 1) == 1);
+//    if (len >= 1 && len <= 0x7e) {
+//      if (c->in_bytes < (int)(1 + 4 * len)) { return; }
+//    } else {
+//      if (c->in_bytes < 4) { return; }
+//      assert (tgln_read_in_lookup (c, &len, 4) == 4);
+//      len = (len >> 8);
+//      if (c->in_bytes < (int)(4 + 4 * len)) { return; }
+//      len = 0x7f;
+//    }
+//
+//    if (len >= 1 && len <= 0x7e) {
+//      assert (tgln_read_in (c, &t, 1) == 1);
+//      assert (t == len);
+//      assert (len >= 1);
+//    } else {
+//      assert (len == 0x7f);
+//      assert (tgln_read_in (c, &len, 4) == 4);
+//      len = (len >> 8);
+//      assert (len >= 1);
+//    }
+//    len *= 4;
+//    int op;
+//    assert (tgln_read_in_lookup (c, &op, 4) == 4);
+//    if (c->methods->execute (TLS, c, op, len) < 0) {
+//      return;
+//    }
+//  }
+}
+
+
+static connection *create_connection(tgl_state *tls, const char *host, int port,
+    tgl_session *session, tgl_dc *dc, mtproto_methods *methods)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    connection *c = new connection(tls, session, dc, methods);
+    c->connect_to_server(host, port);
+    return c;
+}
+
+static int write_out(connection *c, const void *_data, int len)
+{
+    return c->write(_data, len);
+}
+
+static int read_in(connection *c, void *_data, int len)
+{
+    return c->read(_data, len);
+}
+
+static int read_in_lookup(connection *c, void *_data, int len)
+{
     qDebug(__PRETTY_FUNCTION__);
     // read but not change read pointer
-//  unsigned char *data = _data;
-//  if (!len || !c->in_bytes) { return 0; }
-//  assert (len > 0);
-//  if (len > c->in_bytes) {
-//    len = c->in_bytes;
-//  }
-  int x = 0;
-//  struct connection_buffer *b = c->in_head;
-//  while (len) {
-//    int y = b->wptr - b->rptr;
-//    if (y >= len) {
-//      memcpy (data, b->rptr, len);
-//      return x + len;
-//    } else {
-//      memcpy (data, b->rptr, y);
-//      x += y;
-//      data += y;
-//      len -= y;
-//      b = b->next;
-//    }
-//  }
-  return x;
+    return 0;
 }
 
-static void incr_out_packet_num (struct connection *c) {
-    qDebug(__PRETTY_FUNCTION__);
-//  c->out_packet_num ++;
-}
-
-static void conn_free (struct connection *c) {
-    qDebug(__PRETTY_FUNCTION__);
-//  if (c->ip) { tfree_str (c->ip); }
-//  if (c->fail_ev) { event_free (c->fail_ev); }
-//  if (c->read_ev) { event_free (c->read_ev); }
-//  if (c->write_ev) { event_free (c->write_ev); }
-
-//  struct connection_buffer *b = c->out_head;
-//  while (b) {
-//    struct connection_buffer *d = b;
-//    b = b->next;
-//    delete_connection_buffer (d);
-//  }
-//  b = c->in_head;
-//  while (b) {
-//    struct connection_buffer *d = b;
-//    b = b->next;
-//    delete_connection_buffer (d);
-//  }
-
-//  if (c->fd >= 0) { Connections[c->fd] = 0; close (c->fd); }
-//  tfree (c, sizeof (*c));
-}
-
-static struct tgl_dc *get_dc (struct connection *c) {
-        qDebug(__PRETTY_FUNCTION__);
-  return c->dc;
-}
-
-static struct tgl_session *get_session (struct connection *c) {
-        qDebug(__PRETTY_FUNCTION__);
-  return c->session;
-}
-
-void flush_out (struct connection *c) {
+static void incr_out_packet_num(connection *c)
+{
     qDebug(__PRETTY_FUNCTION__);
 }
 
-struct connection *create_connection (struct tgl_state *TLS, const char *host, int port, struct tgl_session *session, struct tgl_dc *dc, struct mtproto_methods *methods) {
-        qDebug(__PRETTY_FUNCTION__);
-//  struct connection *c = talloc0 (sizeof (*c));
-//  c->TLS = TLS;
-//  c->ip = tstrdup (host);
-//  c->port = port;
-//
-//  int fd = my_connect (c, c->ip);
-//  if (fd < 0) {
-//    vlogprintf (E_ERROR, "Can not connect to %s:%d %m\n", host, port);
-//    tfree (c, sizeof (*c));
-//    return 0;
-//  }
-//
-//  c->fd = fd;
-//  c->state = conn_connecting;
-//  c->last_receive_time = tglt_get_double_time ();
-//  c->flags = 0;
-//  assert (!Connections[fd]);
-//  Connections[fd] = c;
-//
-//  c->ping_ev = evtimer_new (TLS->ev_base, ping_alarm, c);
-//  c->fail_ev = evtimer_new (TLS->ev_base, fail_alarm, c);
-//  c->write_ev = event_new (TLS->ev_base, c->fd, EV_WRITE, conn_try_write, c);
-//
-//  struct timeval tv = {5, 0};
-//  c->read_ev = event_new (TLS->ev_base, c->fd, EV_READ | EV_PERSIST, conn_try_read, c);
-//  event_add (c->read_ev, &tv);
-//
-//  start_ping_timer (c);
-//
-//  c->dc = dc;
-//  c->session = session;
-//  c->methods = methods;
-//
-//  char byte = 0xef;
-//  assert (tgln_write_out (c, &byte, 1) == 1);
-//  tgln_flush_out (c);
-//
-//  return c;
-        return NULL;
+static tgl_dc *get_dc(connection *c)
+{
+    return c->dc;
 }
 
-tgl_net_methods conn_methods = {
-  write_out,
-  read_in,
-  read_in_lookup,
-  flush_out,
-  incr_out_packet_num,
-  conn_free,
-  get_dc,
-  get_session,
-  create_connection
-};
+static tgl_session *get_session(connection *c)
+{
+    return c->session;
+}
+
+static void flush_out(connection *c)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    c->flush();
+}
+
+static void conn_free(connection *c)
+{
+    qDebug(__PRETTY_FUNCTION__);
+    delete c;
+}
+
+
+#include "qtconnection.moc"
